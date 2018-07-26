d5b6e63df32f072fff3bdc4411e452eeab618402
Tamara <cocidoci@gmail.com>
2018-07-26 14:08:56 +0300

    added webdriver singleton

--- src/main/java/WebDriverSingleton.java
+++ src/main/java/WebDriverSingleton.java
@@ -1,2 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebDriverSingleton {
+    private static WebDriver driver;
+
+    private WebDriverSingleton() {
+    }
+
+    public static WebDriver getWebDriverInstance() {
+        if (null == driver) {
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
 }
